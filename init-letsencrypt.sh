9e2e9ef7d6ef5a08f75b0e7d10f84b321cca3c07
nguyenvuong310 <trungvuong2169@gmail.com>
2025-03-31 22:08:13 +0700

    test

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 
 domains="tvbix.xyz" # Add your domain(s)
 rsa_key_size=4096
-data_path="certbot"
+data_path="./webserver/certbot"
 email="trungvuong310@gmail.com" # Adding a valid address is strongly recommended
 staging=0                # Set to 1 if you're testing your setup to avoid hitting request limits
 
